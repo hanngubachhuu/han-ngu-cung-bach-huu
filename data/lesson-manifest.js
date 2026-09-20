ac0852348b3d076f6b7f8935536cfb7b83d27302
hanngubachhuu <bachhuu1809@gmail.com>
2026-09-20 07:27:29 +0700

    Chuẩn hóa tên hiển thị Bài 2 trong manifest

--- data/lesson-manifest.js
+++ data/lesson-manifest.js
@@ -2,5 +2,5 @@
 window.HAN_NGU_DATA = window.HAN_NGU_DATA || {};
 window.HAN_NGU_DATA.manifest = [
   {id:"hsk2_bai1_jiuyue",level:2,lessonNo:1,titleZh:"九月去北京旅游最好",titleVi:"Tháng 9 đi Bắc Kinh du lịch là tốt nhất",href:"bvnh2-1.html",data:"data/lessons/hsk2/bai1.js"},
-  {id:"hsk2_bai2_liudian_qichuang",level:2,lessonNo:2,titleZh:"我每天六点起床",titleVi:"Bài 2 · Giáo trình chuẩn HSK 2",href:"bai2_index.html",data:"data/lessons/hsk2/bai2.js"}
+  {id:"hsk2_bai2_liudian_qichuang",level:2,lessonNo:2,titleZh:"我每天六点起床",titleVi:"Mỗi ngày tôi dậy lúc 6 giờ",href:"bai2_index.html",data:"data/lessons/hsk2/bai2.js"}
 ];
