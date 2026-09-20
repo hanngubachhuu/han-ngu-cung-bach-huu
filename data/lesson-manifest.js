91e3fb3d61c55a212f6b0f0429d730fab6daf43f
hanngubachhuu <bachhuu1809@gmail.com>
2026-09-20 09:10:35 +0700

    data(hsk2): register all 15 canonical lessons

--- data/lesson-manifest.js
+++ data/lesson-manifest.js
@@ -1,6 +1,139 @@
 /* Auto-assembled lesson manifest. Add a lesson here when its canonical data file is ready. */
 window.HAN_NGU_DATA = window.HAN_NGU_DATA || {};
 window.HAN_NGU_DATA.manifest = [
-  {id:"hsk2_bai1_jiuyue",level:2,lessonNo:1,titleZh:"九月去北京旅游最好",titleVi:"Tháng 9 đi Bắc Kinh du lịch là tốt nhất",href:"bvnh2-1.html",data:"data/lessons/hsk2/bai1.js"},
-  {id:"hsk2_bai2_liudian_qichuang",level:2,lessonNo:2,titleZh:"我每天六点起床",titleVi:"Mỗi ngày tôi dậy lúc 6 giờ",href:"bai2_index.html",data:"data/lessons/hsk2/bai2.js"}
+  {
+    "id": "hsk2_bai1_jiuyue",
+    "level": 2,
+    "lessonNo": 1,
+    "titleZh": "九月去北京旅游最好",
+    "titleVi": "Tháng 9 đi Bắc Kinh du lịch là tốt nhất",
+    "href": "bvnh2-1.html",
+    "data": "data/lessons/hsk2/bai1.js"
+  },
+  {
+    "id": "hsk2_bai2_liudian_qichuang",
+    "level": 2,
+    "lessonNo": 2,
+    "titleZh": "我每天六点起床",
+    "titleVi": "Mỗi ngày tôi dậy lúc 6 giờ",
+    "href": "bai2_index.html",
+    "data": "data/lessons/hsk2/bai2.js"
+  },
+  {
+    "id": "hsk2_bai3_zuobian_hongse",
+    "level": 2,
+    "lessonNo": 3,
+    "titleZh": "左边那个红色的是我的",
+    "titleVi": "Cái màu đỏ bên trái kia là của tôi",
+    "href": "bai3_index.html",
+    "data": "data/lessons/hsk2/bai3.js"
+  },
+  {
+    "id": "hsk2_bai4_zhege_gongzuo",
+    "level": 2,
+    "lessonNo": 4,
+    "titleZh": "这个工作是他帮我介绍的",
+    "titleVi": "Công việc này là anh ấy giới thiệu giúp tôi",
+    "href": "bai4_index.html",
+    "data": "data/lessons/hsk2/bai4.js"
+  },
+  {
+    "id": "hsk2_bai5_jiu",
+    "level": 2,
+    "lessonNo": 5,
+    "titleZh": "就买这件吧",
+    "titleVi": "Mua chiếc áo này đi",
+    "href": "bai5_index.html",
+    "data": "data/lessons/hsk2/bai5.js"
+  },
+  {
+    "id": "hsk2_bai6_zenme",
+    "level": 2,
+    "lessonNo": 6,
+    "titleZh": "你怎么不吃了？",
+    "titleVi": "Sao bạn không ăn nữa?",
+    "href": "bai6_index.html",
+    "data": "data/lessons/hsk2/bai6.js"
+  },
+  {
+    "id": "hsk2_bai7_li",
+    "level": 2,
+    "lessonNo": 7,
+    "titleZh": "你家离公司远吗？",
+    "titleVi": "Nhà bạn có cách công ty xa không?",
+    "href": "bai7_index.html",
+    "data": "data/lessons/hsk2/bai7.js"
+  },
+  {
+    "id": "hsk2_bai8",
+    "level": 2,
+    "lessonNo": 8,
+    "titleZh": "让我想想再告诉你",
+    "titleVi": "Để tôi nghĩ rồi nói cho bạn biết",
+    "href": "bai8_index.html",
+    "data": "data/lessons/hsk2/bai8.js"
+  },
+  {
+    "id": "hsk2_bai9",
+    "level": 2,
+    "lessonNo": 9,
+    "titleZh": "题太多，我没做完",
+    "titleVi": "Bài nhiều quá, tôi chưa làm xong",
+    "href": "bai9_index.html",
+    "data": "data/lessons/hsk2/bai9.js"
+  },
+  {
+    "id": "hsk2_bai10",
+    "level": 2,
+    "lessonNo": 10,
+    "titleZh": "别找了，手机在桌子上呢",
+    "titleVi": "Đừng tìm nữa, điện thoại ở trên bàn",
+    "href": "bai10_index.html",
+    "data": "data/lessons/hsk2/bai10.js"
+  },
+  {
+    "id": "hsk2_bai11",
+    "level": 2,
+    "lessonNo": 11,
+    "titleZh": "他比我大三岁",
+    "titleVi": "Anh ấy lớn hơn tôi ba tuổi",
+    "href": "bai11_index.html",
+    "data": "data/lessons/hsk2/bai11.js"
+  },
+  {
+    "id": "hsk2_bai12",
+    "level": 2,
+    "lessonNo": 12,
+    "titleZh": "你穿得太少了",
+    "titleVi": "Bạn mặc ít quá",
+    "href": "bai12_index.html",
+    "data": "data/lessons/hsk2/bai12.js"
+  },
+  {
+    "id": "hsk2_bai13",
+    "level": 2,
+    "lessonNo": 13,
+    "titleZh": "门开着呢",
+    "titleVi": "Cửa đang mở đấy",
+    "href": "bai13_index.html",
+    "data": "data/lessons/hsk2/bai13.js"
+  },
+  {
+    "id": "hsk2_bai14",
+    "level": 2,
+    "lessonNo": 14,
+    "titleZh": "你看过那个电影吗",
+    "titleVi": "Bạn đã từng xem bộ phim đó chưa?",
+    "href": "bai14_index.html",
+    "data": "data/lessons/hsk2/bai14.js"
+  },
+  {
+    "id": "hsk2_bai15",
+    "level": 2,
+    "lessonNo": 15,
+    "titleZh": "新年就要到了",
+    "titleVi": "Năm mới sắp đến rồi",
+    "href": "bai15_index.html",
+    "data": "data/lessons/hsk2/bai15.js"
+  }
 ];
